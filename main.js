89be6845024b4e8d05a6e3b8b724e8190c45c3d1
Raven Askew <raven.askew@blueacorn.com>
2016-02-01 15:40:37 -0500

    Updated copyright date and added alerts and some notes to JS file

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function ($) {
 
     //This is the start, all inputs add together as long as there is a number in each one (at least a zero).
+    //If input is blank, make the value 0. Something similar to this(maybe on click before doing the .each?: $('#player2 #p2dbl1').val('0');
 
 
     $('#p1total').on('click', function(event) {
@@ -11,7 +12,7 @@ jQuery(document).ready(function ($) {
             $(this).find('input').each(function(i,n) {
                 totalPoints += parseInt($(n).val(),10);
             });
-            alert(totalPoints);
+            alert('Player 1 has ' + totalPoints + ' points');
         });
 
     });
@@ -24,7 +25,7 @@ jQuery(document).ready(function ($) {
             $(this).find('input').each(function(i,n) {
                 totalPoints += parseInt($(n).val(),10);
             });
-            alert(totalPoints);
+            alert('Player 2 has ' + totalPoints + ' points');
         });
 
     });
