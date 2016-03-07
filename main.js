a79293e02f5220f41185f1722f9b8372e99aba91
Raven Askew <raven.askew@blueacorn.com>
2016-03-07 13:11:42 -0500

    Fixed JS to complete score addition by adding a zero if the input is blank. Updated minor styling of header.

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 jQuery(document).ready(function ($) {
 
     //This is the start, all inputs add together as long as there is a number in each one (at least a zero).
+    //MUST - Add in localStorage
     //If input is blank, make the value 0. Something similar to this(maybe on click before doing the .each?: $('#player2 #p2dbl1').val('0');
-
 
     $('#p1total').on('click', function(event) {
         event.preventDefault();
+
+        $('#player1 input').each(function() {
+            if ($(this).val() == "") {
+                $(this).val(0);
+            } else {}
+        });
 
         $('#player1').each(function() {
             var totalPoints = 0;
             $(this).find('input').each(function(i,n) {
                 totalPoints += parseInt($(n).val(),10);
             });
-            alert('Player 1 has ' + totalPoints + ' points');
+            console.log('Player ONE has ' + totalPoints + ' points');
         });
 
     });
@@ -20,12 +26,18 @@ jQuery(document).ready(function ($) {
     $('#p2total').on('click', function(event) {
         event.preventDefault();
 
+        $('#player2 input').each(function() {
+            if ($(this).val() == "") {
+                $(this).val(0);
+            } else {}
+        });
+
         $('#player2').each(function() {
             var totalPoints = 0;
             $(this).find('input').each(function(i,n) {
                 totalPoints += parseInt($(n).val(),10);
             });
-            alert('Player 2 has ' + totalPoints + ' points');
+            console.log('Player TWO has ' + totalPoints + ' points');
         });
 
     });
